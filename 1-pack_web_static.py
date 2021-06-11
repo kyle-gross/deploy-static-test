39294f63115d914df054cfea6c08c51f83ccd50d
kyle-gross <kyg555@outlook.com>
2021-06-11 20:34:43 +0000

    Add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module generates a .tgz file from web_static"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """This function packs up all files from web_static"""
+    try:
+        now = datetime.now()
+
+        tar_archive_name = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        tar_archive_path = "versions/" + tar_archive_name
+
+        local("mkdir -p versions")
+
+        local("tar -czvf " + tar_archive_path + " web_static")
+
+        return tar_archive_path
+
+    except:
+        return None
